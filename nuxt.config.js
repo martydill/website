f04f1f38bad91809cdc8565d8eaa602b467950f6
Marty Dill <martydill@users.noreply.github.com>
2022-07-08 18:16:29 -0700

    Initial commit of website

---
+++ nuxt.config.js
@@ -0,0 +1,46 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'codeninja',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+	'~/assets/css/main.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+	'node_modules/vuenes.css/vuenes.css.js'
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/hello.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<h2>Hello</h2>
+		<div>
+			<a href="./blog/"><i class='nes-icon coin'></i>Blog</a>
+		</div>
+		<div>
+			<a href="https://www.linkedin.com/in/martydill/"><i class='nes-icon linkedin'></i>LinkedIn</a>
+		</div>
+		<div>
+			<a href="https://github.com/martydill"><i class='nes-icon github' />GitHub</a>
+		</div>
+		<div class="footer-container">
+			<div class="footer">
+				Made with <a href="https://nuxtjs.org/">NuxtJS</a> and <a
+					href="https://github.com/jefer590/vuenes.css/">VueNES.css</a>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script></script>
+import Vue from 'vue'
+import VueNesCss from 'vuenes.css'
+
+export default Vue.extend({
+	name: 'HelloPage'
+})
+</script>
